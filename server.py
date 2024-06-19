419d410027afda30a0cce14e22de71cb96828322
BigWolf <119083621+imrsaleh@users.noreply.github.com>
2024-06-19 23:36:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,23 +4,26 @@ from aiohttp import web
 
 from pyscripts import live, proxy, hadeftvauth
 
-ALLOWED_URL = "https://alameedtv.blogspot.com"
+ALLOWED_origin = "https://alameedtv.blogspot.com"
+ALLOWED_referer = "https://alameedtv.blogspot.com/"
 
 async def check_origin_middleware(app, handler):
     async def middleware_handler(request):
         origin = request.headers.get('Origin')
         referer = request.headers.get('Referer')
         
-        if origin != ALLOWED_URL and referer != ALLOWED_URL:
+        if origin != ALLOWED_origin:
             return web.Response(text="Unauthorized", status=403)
-        
+        if referer != ALLOWED_referer:
+            return web.Response(text="Unauthorized", status=403)
+            
         return await handler(request)
     return middleware_handler
 
 async def cors_middleware(app, handler):
     async def middleware_handler(request):
         response = await handler(request)
-        response.headers['Access-Control-Allow-Origin'] = ALLOWED_URL
+        response.headers['Access-Control-Allow-Origin'] = ALLOWED_origin
         response.headers['Access-Control-Allow-Methods'] = '*'
         response.headers['Access-Control-Allow-Headers'] = '*'
         return response
@@ -32,9 +35,6 @@ app.router.add_route('*', '/live/{url:.*}', live.handle_live)
 app.router.add_get('/proxy/{url:.*}', proxy.handle_proxy)
 app.router.add_route('*', '/hadeftv/get/auth', hadeftvauth.hadefauth)
 
-if __name__ == '__main__':
-    port = int(os.getenv("PORT", default=5000))
-    web.run_app(app, host='0.0.0.0', port=port)
 if __name__ == '__main__':
     port = int(os.getenv("PORT", default=5000))
     web.run_app(app, host='0.0.0.0', port=port)
