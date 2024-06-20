fa192c5dea1b8e5ebbccd58780a4479676cd52c4
BigWolf <119083621+imrsaleh@users.noreply.github.com>
2024-06-20 06:25:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ async def check_origin_middleware(app, handler):
 async def cors_middleware(app, handler):
     async def middleware_handler(request):
         response = await handler(request)
-        response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGIN
+        response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = '*'
         response.headers['Access-Control-Allow-Headers'] = '*'
         return response
