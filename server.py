af0b4dbac717635e4c0dbd38bbcae4be19debb44
BigWolf <119083621+imrsaleh@users.noreply.github.com>
2024-06-27 07:26:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,19 @@ import hmac
 import re
 import requests
 from pyscripts import live, proxy, hadeftvauth, shahiddrm
+from pyscripts.rotana_net import (
+    handle_rcinemaksa,
+    handle_rcinemaeg,
+    handle_rcomedy,
+    handle_rclassic,
+    handle_rdrama,
+    handle_rkhalijeah,
+    handle_rlbc,
+    handle_rclip,
+    handle_alressalah,
+    handle_alressalahintl
+)
+
 
 ALLOWED_ORIGIN = "https://www.alameedtv.xyz"
 ALLOWED_REFERER = "https://www.alameedtv.xyz/"
@@ -41,6 +54,19 @@ app.router.add_route('*', '/live/{url:.*}', live.handle_live)
 app.router.add_get('/proxy/{url:.*}', proxy.handle_proxy)
 app.router.add_route('*', '/hadeftv/get/auth', hadeftvauth.hadefauth)
 app.router.add_route('*', '/shahid/api/auth', shahiddrm.shahid_api_auth_handler)
+
+#قنوات روتانا
+# إضافة المسارات الجديدة
+app.router.add_get('/live-hls/rcinemaksa', handle_rcinemaksa)
+app.router.add_get('/live-hls/rcinemaeg', handle_rcinemaeg)
+app.router.add_get('/live-hls/rcomedy', handle_rcomedy)
+app.router.add_get('/live-hls/rclassic', handle_rclassic)
+app.router.add_get('/live-hls/rdrama', handle_rdrama)
+app.router.add_get('/live-hls/rkhalijeah', handle_rkhalijeah)
+app.router.add_get('/live-hls/rlbc', handle_rlbc)
+app.router.add_get('/live-hls/rclip', handle_rclip)
+app.router.add_get('/live-hls/alressalah', handle_alressalah)
+app.router.add_get('/live-hls/alressalahintl', handle_alressalahintl)
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", default=5000))
